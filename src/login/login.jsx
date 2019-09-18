5493f7dfd81d0158568067fc8294402b90543ef6
bastian <k.batakovich@gmail.com>
2019-09-18 13:16:15 +0300

    Refactor component

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import userLogged from '../actions';
 import style from './login.css';
 
@@ -38,46 +39,52 @@ class Login extends Component {
 
 	validateUser() {
 		const { login, password } = this.state;
+		const { history } = this.props;
 		if (login !== 'Admin' && password !== '12345') {
 			this.setState({ error: 'Имя пользователя или пароль введены не верно ' });
 		} else {
 			localStorage.setItem('isLogged', 'true');
+			history.push('/profile');
 		}
 	}
 
 	render() {
 		const { login, password, error } = this.state;
-		return (
-			<div className={style.login}>
-				<form className={style.form} onSubmit={this.onSubmit}>
-					{error ? <p className={style.error}>{error}</p> : null}
-					<label className={style.label}>пользователь</label>
-					<input
-						type="text"
-						className={style.input}
-						placeholder="Введите ваш логин"
-						name="login"
-						value={login}
-						onChange={this.onChange}
-					/>
-					<label className={style.label}>пароль</label>
-					<input
-						type="password"
-						className={style.input}
-						placeholder="Введите ваш пароль"
-						name="password"
-						value={password}
-						onChange={this.onChange}
-					/>
-					<button type="submit" className={style.btn}>войти</button>
-				</form>
-			</div>
-		);
+		if (!localStorage.getItem('isLogged')) {
+			return (
+				<div className={style.login}>
+					<form className={style.form} onSubmit={this.onSubmit}>
+						{error ? <p className={style.error}>{error}</p> : null}
+						<label className={style.label}>пользователь</label>
+						<input
+							type="text"
+							className={style.input}
+							placeholder="Введите ваш логин"
+							name="login"
+							value={login}
+							onChange={this.onChange}
+						/>
+						<label className={style.label}>пароль</label>
+						<input
+							type="password"
+							className={style.input}
+							placeholder="Введите ваш пароль"
+							name="password"
+							value={password}
+							onChange={this.onChange}
+						/>
+						<button type="submit" className={style.btn}>войти</button>
+					</form>
+				</div>
+			);
+		}
+		return <Redirect to="/profile" />;
 	}
 }
 
 Login.propTypes = {
 	onSubmit: PropTypes.func.isRequired,
+	history: PropTypes.objectOf(PropTypes.any.isRequired).isRequired,
 };
 
 
